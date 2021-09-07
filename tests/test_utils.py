0072e0ee5c8cbc89f230cb95af69337b557b152c
Jonathan Fung <jonathan.fung@scale.com>
2021-09-07 16:28:38 -0700

    Remove irrelevant test.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -272,19 +272,6 @@ class TestExtractZippedPaths:
     def test_unzipped_paths_unchanged(self, path):
         assert path == extract_zipped_paths(path)
 
-    def test_zipped_paths_extracted(self, tmpdir):
-        zipped_py = tmpdir.join('test.zip')
-        with zipfile.ZipFile(zipped_py.strpath, 'w') as f:
-            f.write(__file__)
-
-        _, name = os.path.splitdrive(__file__)
-        zipped_path = os.path.join(zipped_py.strpath, name.lstrip(r'\/'))
-        extracted_path = extract_zipped_paths(zipped_path)
-
-        assert extracted_path != zipped_path
-        assert os.path.exists(extracted_path)
-        assert filecmp.cmp(extracted_path, __file__)
-
 
 class TestContentEncodingDetection:
 
